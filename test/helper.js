d32baf9ded7ff7cb12d79c259892fb4ffbd394a2
holyshared <holy.shared.design@gmail.com>
2016-09-05 16:02:23 +0900

    add base64

--- test/helper.js
+++ test/helper.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+
 global.assert = require('power-assert');
+
+global.readFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, buffer) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(buffer);
+    });
+  });
+}
--- test/resizer.js
+++ test/resizer.js
@@ -2,12 +2,26 @@ const Resizer = require('../lib');
 
 describe('Resizer', function () {
   describe('saveAs', function () {
-    beforeEach(function () {
-      this.resizer = Resizer.fromFile('test/fixtures/png-fixture.png');
+    context('when file', function () {
+      beforeEach(function () {
+        this.resizer = Resizer.fromFile('test/fixtures/png-fixture.png');
+      });
+      it('resized image', function () {
+        return this.resizer.saveAs('/tmp/foo.png').then((file) => {
+          assert.ok('/tmp/foo.png' === file);
+        });
+      });
     });
-    it('resized image', function () {
-      return this.resizer.saveAs('/tmp/foo.png').then((file) => {
-        assert.ok('/tmp/foo.png' === file);
+    context('when base64 image', function () {
+      beforeEach(function () {
+        return readFile('test/fixtures/base64.txt').then((buffer) => {
+          this.resizer = Resizer.fromBase64URL(buffer.toString());
+        });
+      });
+      it('resized image', function () {
+        return this.resizer.saveAs('/tmp/base64.png').then((file) => {
+          assert.ok('/tmp/base64.png' === file);
+        });
       });
     });
   });
